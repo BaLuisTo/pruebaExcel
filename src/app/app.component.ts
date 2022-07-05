9ad6c778dffb61d7f8b55c3d9165ff18db189d54
Alejandro Luis Torres <bryan.luis@segurosbolivar.com>
2022-07-05 10:28:16 -0500

    Se agrega imagen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const workbook = XLSX.utils.table_to_book(document.getElementById("table"));
+    console.log(workbook);
+    
+  }
   title = 'pruebaExcel';
 
-  workbook = XLSX.utils.table_to_book(document.getElementById("tableau"));
+  
 
 }
