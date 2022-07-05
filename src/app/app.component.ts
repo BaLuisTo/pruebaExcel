e554c44538c4925aebc79e1839071593b9b6b497
Alejandro Luis Torres <bryan.luis@segurosbolivar.com>
2022-07-05 09:59:51 -0500

    avance 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pruebaExcel';
+
+  workbook = XLSX.utils.table_to_book(document.getElementById("tableau"));
+
 }
