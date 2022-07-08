ba706e52a0c7c28f1755dc9145b269075bc8ced2
marcos.carvajal@segurosbolivar.com-TechCamp <marcos.carvajal@segurosbolivar.com>
2022-07-08 09:02:18 -0500

    maquetacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,78 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { jsPDF } from "jspdf";
-import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
 
+    data: any[];
+
+    cols: any[];
   
   ngOnInit(): void {
-    
-    
-  }
-
-  descargarPDF(){
-
     const doc = new jsPDF("portrait", "pt", 'a4');
-      doc.html(document.getElementById('container'), {
+      doc.html(document.getElementById('contenidoPDF'), {
         x: 0,
         y: 0,
-        margin: [0, 0, 100, 0],
+        margin: [0, 24, 24, 24],
         callback: () => {
-          doc.save();
+         // doc.save();
         },
       });
-    
-
-
-
-    //////////////////////////////////////////////////////////////////////
-    
-    //seleccion de lo que se quiere en el pdf
-    // const DATA = document.getElementById('container');
-
-    // const doc = new jsPDF('p', 'pt', 'a4');
-
-    // const options = {
-    //   background: 'white',
-    //   scale: 3
-    // }
-      // doc.html(document.body, {
-      //         x: 0,
-      //         y: 0,
-      //         autoPaging: 'text',
-      //         margin: [700, 0, 700, 0],
-              
-      //         callback: () => {
-      //           doc.save();
-      //         },
-      //       });
-
-    // html2canvas(DATA, options).then((canvas) => {
-
-    //   const img = canvas.toDataURL('image/PNG');
-
-    //   // Add image Canvas to PDF
-    //   const bufferX = 15;
-    //   const bufferY = 15;
-    //   const imgProps = (doc as any).getImageProperties(img);
-    //   const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
-    //   const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      const pageCount = doc.internal.pages
+      console.log(pageCount);
       
 
-    //   doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
-      
-    //   return doc;}).then((docResult) => {
-    //   docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
-    // });
+    /*DATOS TABLA*/
+      this.data = [
+        {
+            name: 1,
+            author: "prueba1",
+            price: "categoria"
+        },
+        {
+            name: 1,
+            author: "prueba1",
+            price: "categoria"
+        }
+      ]
+
+
+    this.cols = [
+        { field: 'name', header: 'Name' },
+        {field: 'author', header: 'Author' },
+        { field: 'price', header: 'Price' }      
+    ];
+    
   }
-
-  title = 'pruebaExcel';
-
-  
-
 }
